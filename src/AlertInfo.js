741703d69cbb301ed03daae64d5d08f625831380
Ing. Luis Pablo Gallo <luispablo.gallo@gmail.com>
2018-07-20 15:02:35 -0300

    Add close button to info messages

--- src/AlertInfo.js
+++ src/AlertInfo.js
@@ -1,7 +1,13 @@
 var React = require("react"); // eslint-disable-line no-undef
 
 var AlertInfo = function (props) {
-	return React.createElement("div", { className: "alert alert-info", role: "alert" }, props.message);
+	var alertClass = "alert alert-info alert-dismissible fade show";
+	var closeButtonProps = { type: "button", className: "close", "data-dismiss": "alert", "aria-label": "Close" };
+
+	var spanTimes = React.createElement("span", { "aria-hidden": "true", dangerouslySetInnerHTML: { __html: '&times;' } });
+	var buttonClose = React.createElement("button", closeButtonProps, spanTimes);
+
+	return React.createElement("div", { className: alertClass, role: "alert" }, [props.message, buttonClose]);
 };
 
 module.exports = AlertInfo; // eslint-disable-line no-undef
--- test/AlertInfo-test.js
+++ test/AlertInfo-test.js
@@ -12,7 +12,20 @@ test("PanelMessages - AlertInfo - render", assert => {
 	const content = alert.props.children;
 
 	assert.equal(alert.type, "div", "The main HMTL object is a div");
-	assert.equal(alert.props.className, "alert alert-info", "The DIV css class");
-	assert.equal(content, MESSAGE, "The alert panel text");
+	assert.equal(alert.props.className, "alert alert-info alert-dismissible fade show", "The DIV css class");
+	assert.equal(content.length, 2, "Two items inside");
+
+	const message = content[0];
+	const buttonClose = content[1];
+
+	assert.equal(message, MESSAGE, "The alert panel text");
+	assert.equal(buttonClose.type, "button", "The close button");
+	assert.equal(buttonClose.props.className, "close", "The close button class");
+	assert.equal(buttonClose.props["data-dismiss"], "alert", "The close button data-dismiss");
+	assert.equal(buttonClose.props["aria-label"], "Close", "The close button aria-label");
+
+	const spanCloseIcon = buttonClose.props.children;
+
+	assert.equal(spanCloseIcon.type, "span");
 	assert.end();
 });
